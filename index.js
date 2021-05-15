70a0070cd9c57d2feb7f090c297e04027e759bbd
mensha <msksalston@gmail.com>
2021-05-15 18:30:29 -0400

    commit tree 62c97ffa591adb2f7001afa2e6f66963878f977a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat() {
+  return cats.push('Ralph') 
+} 
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop(-1)
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift(0)
+}
+
+function appendCat() {
+    const newCats  = [...cats,'Broom'] 
+     return newCats
+}
+
+function prependCat() {
+    const newCatsToo = ['Arnold', ...cats]
+    return newCatsToo
+}
+
+function removeLastCat() {
+    const subtractLastCat = cats.slice(0, cats.length-1)
+    return subtractLastCat
+}
+
+function removeFirstCat() {
+    const subtractFirstCat = cats.slice(1)
+    return subtractFirstCat
+}
